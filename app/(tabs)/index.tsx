58ef9e00af93d940929e6521da08d2d441f96ea3
Roshan Gurung <roshc10grg@gmail.com>
2025-06-16 16:24:05 +0545

    searching route

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,14 @@
 import SearchBar from "@/components/searchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
+
+   const router = useRouter();
+
+
   return (
     <SafeAreaView className="flex-1 bg-primary">
          <Image source={images.bg} className="absolute z-0 w-full" />
@@ -15,7 +20,10 @@ export default function Index() {
               <Image source={icons.logo} className="w-12 mx-auto mt-10 mb-2"/>
                 
                 <View className="flex-1">
-                      <SearchBar/>
+                      <SearchBar
+                        onPress={() => router.push('/search')}
+                        placeholder="Search for a movie"
+                      />
                 </View>
              
 
--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,13 +1,19 @@
-import { icons } from '@/constants/icons'
-import React from 'react'
-import { Image, TextInput, View } from 'react-native'
-
-const SearchBar = () => {
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
+ interface Props {
+  placeholder?: string;
+  onPress: () => void;
+}
+const SearchBar = ({placeholder,onPress}: Props) => {
   return (
     <View className='flex-row items-center px-4 py-4 border rounded-full border-primary bg-secondary'>
        <Image source={icons.search} className="size-5" tintColor='#ab8bff' resizeMode='contain' />
          <TextInput
-          placeholder='Search a movie'
+          onPress={onPress}
+          placeholder={placeholder}
+          value=''
+          onChange={() => {}}
           placeholderTextColor='#ab8bff'
           className='flex-1 ml-2 text-base text-white'
     
